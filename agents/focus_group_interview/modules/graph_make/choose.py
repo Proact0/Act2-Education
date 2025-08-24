267137d5c506fc6a140c0f18103a538ec583d277
sondonghup <sondonghup@naver.com>
2025-08-25 01:59:14 +0900

    feat: add graph choose

---
+++ agents/focus_group_interview/modules/graph_make/choose.py
@@ -0,0 +1,29 @@
+from google import genai
+import enum
+
+class TalkType(enum.Enum):
+    NORMAL = "normal"
+    FREETALK = "freetalk"
+    SEQUENCE = "sequence"
+
+class StartGraph():
+    def __init__(self):
+        
+        self.client = genai.Client()
+
+    def get_type(self, theme):
+
+        response = self.client.models.generate_content(
+            model="gemini-2.5-flash",
+            contents=f"{theme}를 보고 해결 하기에 적합한 talk type을 정해주세요",
+            config={
+                "response_mime_type": "application/json",
+                "response_schema": TalkType,
+                "temperature": 0.0,
+                "seed": 42,
+            }
+        )
+
+        return response
+
+
